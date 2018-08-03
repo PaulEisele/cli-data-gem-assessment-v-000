e9f9186850c3c65fb8998358be10dd428f6ff5ca
paulheisele@gmail.com <paulheisele@gmail.com>
2018-08-03 20:38:06 +0000

    automatically backed up by learn

--- lib/find_recipe/recipe.rb
+++ lib/find_recipe/recipe.rb
@@ -20,19 +20,6 @@ class FindRecipe::Recipe
     self.all[id]
   end
 
-  def title
-    @title ||= i.css("a.full-item-title").text.strip
-  end
-
-  def url
-    @url ||= "https://www.delish.com"+i.css("a.full-item-title").attr("href").value
-  end
-
-  def description
-    binding.pry
-    @description ||= i.css("div.full-item-dek p").text.strip
-  end
-
   def site
     @site ||=Nokogiri::HTML(open(self.url))
   end
